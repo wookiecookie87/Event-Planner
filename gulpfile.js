221e5926086f4bcf03c3588ed5b50147c43ae705
Wookiecookie <wookiecookie87@gmail.com>
2016-07-14 00:10:12 +0900

    complete

--- gulpfile.js
+++ gulpfile.js
@@ -16,44 +16,49 @@ var autoprefixer = require("gulp-autoprefixer");
 var minify = require("gulp-minify");
 //var browserSync = require("browser-sync'").create();
 var eslint = require("gulp-eslint");
+var concat = require("gulp-concat");  
+var rename = require("gulp-rename");  
+var uglify = require("gulp-uglify"); 
+var uglifycss = require("gulp-uglifycss");
 
 gulp.task("default", ["styles", "lint"], function() {
-	// browserSync.init({
- //        server: {
-	// 		baseDir: "./"
- //        }
-	// });
-	console.log("hello gulp");
+
 	gulp.watch("sass/**/*.scss", ["styles"]);
-	//gulp.watch("js/**/*.js", ["lint"]);
-	gulp.watch("js/**/*.js", ["minify"]);
-	//gulp.watch("./*.html").on("change", browserSync.reload);
+	gulp.watch("js/**/*.js", ["lint"]);
+	gulp.watch("js/**/*.js", ["scripts"]);
+	
 });
 
 gulp.task("styles", function(){
-	console.log("style run");
+	
 	gulp.src("sass/**/*.scss")
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer({
 			browsers: ["last 2 versions"]//of every popuplar browsers
 		}))
-		.pipe(gulp.dest("./css"));
+		.pipe(gulp.dest("./css"))
+		.pipe(uglifycss({
+			"maxLineLen": 80,
+			"uglyComments": true
+		}))
+		.pipe(gulp.dest("./dist/"));
 		//.pipe(browserSync.stream());
 });
 
+gulp.task("scripts", function() {  
+	return gulp.src("js/**/*.js")
+        .pipe(concat("scripts.js"))
+        .pipe(gulp.dest("./dist"))
+        .pipe(rename("scripts.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist"));
+});
 
-gulp.task("minify", function(){
-	gulp.src("js/**/*.js")
-		.pipe(minify({
-			ext:{
-				src:"-debug.js",
-				min:".js"
-			},
-			exclude: ["tasks"],
-			ignoreFiles: [".combo.js", "-min.js"]
-		}))
-		.pipe(gulp.dest("./min.js"));
-
+gulp.task("css", function() {  
+	return gulp.src("css/**/*.css")
+        .pipe(uglify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("./dist"));
 });
 
 gulp.task("lint", function() {
@@ -61,4 +66,15 @@ gulp.task("lint", function() {
 				.pipe(eslint())
 				.pipe(eslint.format())
 				.pipe(eslint.failOnError());
+});
+
+
+gulp.task("minify", function(){
+	gulp.src("dist/**/*.js")
+		.pipe(minify({
+			ext:{
+				min:".js"
+			}
+		}))
+		.pipe(gulp.dest("./min.js"));
 });
